66f1a1fb6e10fd9392c5406008eecb32d9f8bc40
dzakyputra45 <dzakypratama45@gmail.com>
2024-05-11 09:51:38 +0700

    Membuat void POP untuk menghapus element paling atas dari tumpukan

--- Stackusinglinkedlist.cpp
+++ Stackusinglinkedlist.cpp
@@ -31,4 +31,13 @@ public:
         cout << "Push Value: " << value << endl;
         return value;
     }
+
+    //Pop operation: Remove the topmost element from the stack
+    void pop(){
+        if (isEmpty ()){
+            cout << "Stack is empty " << endl;
+        }
+        cout << "Poppes value: " << top->data << endl;
+        top = top->next;
+    }
 };
