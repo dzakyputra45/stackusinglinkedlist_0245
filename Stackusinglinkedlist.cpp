fec728f1b17e168cffa5619551d8abccd5d0300e
dzakyputra45 <dzakypratama45@gmail.com>
2024-05-11 09:57:10 +0700

    Melakukan perulangan hingga node terakhir sekaligus mencetak nilainya

--- Stackusinglinkedlist.cpp
+++ Stackusinglinkedlist.cpp
@@ -45,7 +45,14 @@ public:
         if (top == NULL) {
             cout << "List is empty." << endl;
         }
-        
+        else {
+            Node * current = top;
+            while (current != NULL ){
+                    cout << current->data << " " << endl;
+                    current = current->next;
+            }
+            cout << endl;
+        }
         
     }    
 };
